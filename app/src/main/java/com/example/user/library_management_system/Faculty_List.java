048177645596cb75aa17c517032761403d84f2c9
sohan160115 <mdsohan838@gmail.com>
2019-03-31 15:38:38 +0600

    Reading data from Realtime database

--- app/src/main/java/com/example/user/library_management_system/Faculty_List.java
+++ app/src/main/java/com/example/user/library_management_system/Faculty_List.java
@@ -2,8 +2,12 @@ package com.example.user.library_management_system;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class Faculty_List extends AppCompatActivity {
     private ListView listView;
@@ -15,8 +19,21 @@ public class Faculty_List extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty__list);
       listView = (ListView) findViewById(R.id.Listviewid);
-        String[] FacultyList= getResources().getStringArray(R.array.Faculty_List);
+        final String[] FacultyList= getResources().getStringArray(R.array.Faculty_List);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(Faculty_List.this,R.layout.sample_view,R.id.textViewId,FacultyList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(FacultyList[position]==FacultyList[0]) {
+
+                    Intent intent = new Intent(getApplicationContext(), CSE_BOOK.class);
+                    startActivity(intent);
+                }
+                
+            }
+        });
+
+
     }
 }
